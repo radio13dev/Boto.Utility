973878da91d6b9b88b9d1d81ba37b9a9954a103e
Ben Wheatley <radio13dev@gmail.com>
2025-11-11 10:25:18 +1000

    mathu made pure, added lerprepeat method

--- Assets/root/Runtime/mathu.cs
+++ Assets/root/Runtime/mathu.cs
@@ -1,33 +1,38 @@
 using System;
+using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using Unity.Mathematics;
 using UnityEngine;
 
 public static class mathu
 {
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float MoveTowards(float current, float target, float maxDelta)
     {
         return math.abs((double)target - (double)current) <= maxDelta ? target : current + math.sign(target - current) * maxDelta;
     }
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float3 MoveTowards(float3 current, float3 target, float maxDelta)
     {
         return math.distancesq(target,current) <= maxDelta*maxDelta ? target : current + math.normalize(target - current) * maxDelta;
     }
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float2 MoveTowards(float2 current, float2 target, float maxDelta)
     {
         return math.distancesq(target,current) <= maxDelta*maxDelta ? target : current + math.normalize(target - current) * maxDelta;
     }
     
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool Contains(this float2 range, float v)
     {
         return range.x < v && v <= range.y;
     }
     
-    
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float3 perpendicular(float3 n)
     {
@@ -51,6 +56,7 @@ public static class mathu
         return math.normalize(tangent);
     }
     
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int modabs(int x, int m)
     {
@@ -58,6 +64,7 @@ public static class mathu
         if (r >= 0) return r;
         return r + m;
     }
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float modabs(float x, float m)
     {
@@ -65,11 +72,13 @@ public static class mathu
         if (r >= 0) return r;
         return r + m;
     }
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector2Int modabs(Vector2Int x, Vector2Int m)
     {
         return new Vector2Int((int)modabs(x.x,m.x), (int)modabs(x.y,m.y));
     }
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float lerpangle(float a, float b, float t)
     {
@@ -78,6 +87,16 @@ public static class mathu
             num -= math.PI;
         return a + num * math.clamp(t,0,1);
     }
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float lerprepeat(float a, float b, float t, float length)
+    {
+        var num = repeat(b - a, length*2);
+        if (num > length)
+            num -= length*2;
+        return a + num * math.clamp(t,0,1);
+    }
+    [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float repeat(float t, float length)
     {
